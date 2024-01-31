64652db288b0f5a3040754a0e70f48c9a00cce36
AlonBarShimon <alon.barshimon@post.runi.ac.il>
2024-01-31 18:04:15 +0200

    check1

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,16 +1,49 @@
 public class ArrayOps {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         
+
     }
     
     public static int findMissingInt (int [] array) {
         // Write your code here:
-        return -1;
+        int n = array.length ;
+        int a = 0 ;
+        for (int i = 0 ; i <= n  ; i++) {
+            boolean missing = false;
+            for (int j = 0; j < n; j++) {
+                if (i == array[j]) {
+                    missing = true;
+                    break;
+                }
+            }
+            if (!missing) {
+                a = i;
+                break;
+            }
+        }
+
+        return a;
     }
 
     public static int secondMaxValue(int [] array) {
         // Write your code here:
-        return 0;
+        int n = array.length ;
+
+        if(n<2)return -1;
+
+        int biggest = array[0];
+        int second = array[0];
+
+        for (int i = 0 ; i < n ; i ++) {
+
+            if (biggest < array[i]){
+                second = biggest;
+                biggest = array[i];
+           }
+             else if (second<array[i] && array[i]!=biggest) second = array[i] ;
+        }
+
+        return second;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
